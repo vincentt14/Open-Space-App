2b136c40b0ad81dfb1327b0b55330c3583ed9348
vincentt14 <vincent14@student.umn.ac.id>
2023-01-05 20:55:30 +0700

    Adding Action & Reducer for isPreload state

--- src/states/isPreload/action.js
+++ src/states/isPreload/action.js
@@ -1,3 +1,32 @@
-/**
- * @TODO: Define all the actions (creator) for the isPreLoad state
- */
+import api from '../../utils/api';
+
+const ActionType = {
+  SET_IS_PRELOAD: 'SET_IS_PRELOAD',
+};
+
+function setIsPreloadActionCreator(isPreLoad) {
+  return {
+    type: ActionType.SET_IS_PRELOAD,
+    payload: {
+      isPreLoad,
+    },
+  };
+}
+
+function asyncPreloadProcess() {
+  return async (dispatch) => {
+    try {
+      // Preload process
+      const authUser = await api.getOwnProfile();
+      dispatch(setIsPreloadActionCreator(authUser));
+    } catch (error) {
+      // Fallback process
+      dispatch(setIsPreloadActionCreator(null));
+    } finally {
+      // End preload process
+      dispatch(setIsPreloadActionCreator(false));
+    }
+  };
+}
+
+export { ActionType, setIsPreloadActionCreator, asyncPreloadProcess };
--- src/states/isPreload/reducer.js
+++ src/states/isPreload/reducer.js
@@ -1,3 +1,10 @@
-/**
- * @TODO: Define reducer for the isPreLoad state
- */
+import { ActionType } from './action';
+
+function isPreloadReducer(isPreLoad = true, action = {}) {
+  switch (action.type) {
+    case ActionType.SET_IS_PRELOAD:
+      return action.payload.isPreLoad;
+    default:
+      return isPreLoad;
+  }
+}
