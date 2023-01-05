857b301a0e94e105b20d42203f3897f66cef4484
vincentt14 <vincent14@student.umn.ac.id>
2023-01-05 21:39:05 +0700

    Adding Action & Reducer for users state

--- src/states/users/action.js
+++ src/states/users/action.js
@@ -1,3 +1,26 @@
-/**
- * @TODO: Define all the actions (creator) for the users state
- */
+import api from '../../utils/api';
+
+const ActionType = {
+  RECIEVE_USERS: 'RECIEVE_USERS',
+};
+
+function recieveUsersActionCreator(users) {
+  return {
+    type: ActionType.RECIEVE_USERS,
+    payload: {
+      users,
+    },
+  };
+}
+
+function asyncRegisterUser({ id, name, password }) {
+  return async () => {
+    try {
+      await api.register({ id, name, password });
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+}
+
+export { ActionType, recieveUsersActionCreator, asyncRegisterUser };
--- src/states/users/reducer.js
+++ src/states/users/reducer.js
@@ -1,3 +1,12 @@
-/**
- * @TODO: Define reducer for the users state
- */
+import { ActionType } from './action';
+
+function usersReducer(users = [], action = {}) {
+  switch (action.type) {
+    case ActionType.RECIEVE_USERS:
+      return action.payload.users;
+    default:
+      return users;
+  }
+}
+
+export default usersReducer;
