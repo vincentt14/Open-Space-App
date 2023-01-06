84dd1b5680c9a3f314cbe2d61603f22abd958952
vincentt14 <vincent14@student.umn.ac.id>
2023-01-06 20:45:21 +0700

    adding sharedAction and ReduxStore

--- src/states/shared/action.js
+++ src/states/shared/action.js
@@ -1,3 +1,19 @@
-/**
- * @TODO: Define all the actions (creator) that uses a combination of actions from various domain
- */
+import api from '../../utils/api';
+import { recieveTalksActionCreator } from '../talks/action';
+import { recieveUsersActionCreator } from '../users/action';
+
+function asyncPopulateUsersAndTalks() {
+  return async (dispatch) => {
+    try {
+      const users = await api.getAllUsers();
+      const talks = await api.getAllTalks();
+
+      dispatch(recieveUsersActionCreator(users));
+      dispatch(recieveTalksActionCreator(talks));
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+}
+
+export default { asyncPopulateUsersAndTalks };
